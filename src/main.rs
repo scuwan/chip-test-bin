be0ac39191a5655c00fdfc0b2671742ad9ed2979
scuwan <215587407@qq.com>
2024-12-10 17:23:51 +0800

    fix: use valid epoch hash string

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,21 @@ async fn main() -> anyhow::Result<()> {
     let getter = aleo_program_service::ProgramGetter::new(server_addr)
         .await
         .unwrap();
-    if let Ok((program, consts)) = getter.get_program("epoch_hash".to_string()).await {
+    if let Ok((program, consts)) = getter
+        .get_program("ab1lnzh5cvsdnhtgcurh4gdeckw68k47f7ua0xfjln5nxwmfg5r9cpskff0vf".to_string())
+        .await
+    {
         println!("{:?} - {:?}", program, consts);
     }
     drop(getter);
-    match get_program(server_addr, "epoch_hash".to_string()).await {
+    match get_program(
+        server_addr,
+        "ab1lnzh5cvsdnhtgcurh4gdeckw68k47f7ua0xfjln5nxwmfg5r9cpskff0vf".to_string(),
+    )
+    .await
+    {
         Ok((program, consts)) => println!("{:?} - {:?}", program, consts),
         Err(error) => println!("{}", error.to_string()),
     }
     Ok(())
 }
-
